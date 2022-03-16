984c6a887413ad5db31678e6651d8e3282864745
lodashreload <icarusjapan@gmail.com>
2022-03-16 11:59:22 -0400

    making the NewArticle react file reusabl;e and customized with props

--- src/component/NewsArticle.tsx
+++ src/component/NewsArticle.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 // export -- is so we can use the outside of this file
-export function NewsArticle() {
-  return <div>Something</div>
+export function NewsArticle(props) {
+  return (
+    <article className="intro-article">
+      <h2 className="article-title">{props.title}</h2>
+      <p>{props.body}</p>
+      <a className="read-more" href="#here">
+        read more about {props.title}
+      </a>
+    </article>
+  )
 }
